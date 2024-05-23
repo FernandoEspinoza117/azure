831bd7d5d1ca80e130b63a21eefd8c0607c8bc29
HenryKH10 <144644114+HenryKH10@users.noreply.github.com>
2024-05-22 22:49:05 -0600

    Avances del proyecto

--- Azure/servicios.Designer.cs
+++ Azure/servicios.Designer.cs
@@ -28,12 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(servicios));
+            this.Tipo_Seguridad = new System.Windows.Forms.ComboBox();
+            this.Tipo_DevOps = new System.Windows.Forms.ComboBox();
+            this.Tipo_BD = new System.Windows.Forms.ComboBox();
+            this.Tipo_Redes = new System.Windows.Forms.ComboBox();
+            this.Tipo_Almacenamiento = new System.Windows.Forms.ComboBox();
+            this.Tipos_Computacion = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tipo_Seguridad
+            // 
+            this.Tipo_Seguridad.DisplayMember = "tipo_usuario";
+            this.Tipo_Seguridad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipo_Seguridad.FormattingEnabled = true;
+            this.Tipo_Seguridad.Location = new System.Drawing.Point(702, 297);
+            this.Tipo_Seguridad.Name = "Tipo_Seguridad";
+            this.Tipo_Seguridad.Size = new System.Drawing.Size(121, 24);
+            this.Tipo_Seguridad.TabIndex = 54;
+            this.Tipo_Seguridad.ValueMember = "id_tipo_usuario";
+            // 
+            // Tipo_DevOps
+            // 
+            this.Tipo_DevOps.DisplayMember = "tipo_usuario";
+            this.Tipo_DevOps.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipo_DevOps.FormattingEnabled = true;
+            this.Tipo_DevOps.Location = new System.Drawing.Point(119, 394);
+            this.Tipo_DevOps.Name = "Tipo_DevOps";
+            this.Tipo_DevOps.Size = new System.Drawing.Size(121, 24);
+            this.Tipo_DevOps.TabIndex = 53;
+            this.Tipo_DevOps.ValueMember = "id_tipo_usuario";
+            // 
+            // Tipo_BD
+            // 
+            this.Tipo_BD.DisplayMember = "tipo_usuario";
+            this.Tipo_BD.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipo_BD.FormattingEnabled = true;
+            this.Tipo_BD.Location = new System.Drawing.Point(273, 250);
+            this.Tipo_BD.Name = "Tipo_BD";
+            this.Tipo_BD.Size = new System.Drawing.Size(121, 24);
+            this.Tipo_BD.TabIndex = 52;
+            this.Tipo_BD.ValueMember = "id_tipo_usuario";
+            // 
+            // Tipo_Redes
+            // 
+            this.Tipo_Redes.DisplayMember = "tipo_usuario";
+            this.Tipo_Redes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipo_Redes.FormattingEnabled = true;
+            this.Tipo_Redes.Location = new System.Drawing.Point(503, 241);
+            this.Tipo_Redes.Name = "Tipo_Redes";
+            this.Tipo_Redes.Size = new System.Drawing.Size(121, 24);
+            this.Tipo_Redes.TabIndex = 51;
+            this.Tipo_Redes.ValueMember = "id_tipo_usuario";
+            // 
+            // Tipo_Almacenamiento
+            // 
+            this.Tipo_Almacenamiento.DisplayMember = "tipo_usuario";
+            this.Tipo_Almacenamiento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipo_Almacenamiento.FormattingEnabled = true;
+            this.Tipo_Almacenamiento.Location = new System.Drawing.Point(428, 407);
+            this.Tipo_Almacenamiento.Name = "Tipo_Almacenamiento";
+            this.Tipo_Almacenamiento.Size = new System.Drawing.Size(121, 24);
+            this.Tipo_Almacenamiento.TabIndex = 50;
+            this.Tipo_Almacenamiento.ValueMember = "id_tipo_usuario";
+            // 
+            // Tipos_Computacion
+            // 
+            this.Tipos_Computacion.DisplayMember = "tipo_usuario";
+            this.Tipos_Computacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tipos_Computacion.FormattingEnabled = true;
+            this.Tipos_Computacion.Location = new System.Drawing.Point(51, 222);
+            this.Tipos_Computacion.Name = "Tipos_Computacion";
+            this.Tipos_Computacion.Size = new System.Drawing.Size(121, 24);
+            this.Tipos_Computacion.TabIndex = 49;
+            this.Tipos_Computacion.ValueMember = "id_tipo_usuario";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label7.Location = new System.Drawing.Point(417, 383);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 16);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Servicios de Almacenamiento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.GhostWhite;
+            this.label6.Location = new System.Drawing.Point(129, 370);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 16);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Servicios de devOps\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.GhostWhite;
+            this.label5.Location = new System.Drawing.Point(690, 278);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 16);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Servicios de Seguridad ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.GhostWhite;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(519, 212);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 16);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Servicios de Redes\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(271, 224);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 16);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Servicios de Base De Datos ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(49, 201);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 16);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Servicios de Computación";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(119, 278);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 42;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(429, 291);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 41;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(267, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 63);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "SERVICIOS";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(727, 20);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(124, 87);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 39;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(702, 185);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 38;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(276, 134);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 37;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(503, 121);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 36;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Firebrick;
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(54, 107);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 35;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(851, 433);
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
+            // 
+            // servicios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(873, 455);
+            this.Controls.Add(this.Tipo_Seguridad);
+            this.Controls.Add(this.Tipo_DevOps);
+            this.Controls.Add(this.Tipo_BD);
+            this.Controls.Add(this.Tipo_Redes);
+            this.Controls.Add(this.Tipo_Almacenamiento);
+            this.Controls.Add(this.Tipos_Computacion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "servicios";
             this.Text = "servicios";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox Tipo_Seguridad;
+        private System.Windows.Forms.ComboBox Tipo_DevOps;
+        private System.Windows.Forms.ComboBox Tipo_BD;
+        private System.Windows.Forms.ComboBox Tipo_Redes;
+        private System.Windows.Forms.ComboBox Tipo_Almacenamiento;
+        private System.Windows.Forms.ComboBox Tipos_Computacion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Azure/usuarios.Designer.cs
+++ Azure/usuarios.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(usuarios));
             this.btniniciar = new System.Windows.Forms.Button();
             this.ckmostrar = new System.Windows.Forms.CheckBox();
             this.txtcontra = new System.Windows.Forms.TextBox();
             this.txtusuario = new System.Windows.Forms.TextBox();
             this.lblcontra = new System.Windows.Forms.Label();
             this.lblusuario = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btniniciar
             // 
             this.btniniciar.BackColor = System.Drawing.Color.AliceBlue;
+            this.btniniciar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
             this.btniniciar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btniniciar.Location = new System.Drawing.Point(200, 175);
+            this.btniniciar.Location = new System.Drawing.Point(268, 226);
+            this.btniniciar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btniniciar.Name = "btniniciar";
-            this.btniniciar.Size = new System.Drawing.Size(75, 23);
+            this.btniniciar.Size = new System.Drawing.Size(100, 28);
             this.btniniciar.TabIndex = 17;
             this.btniniciar.Text = "Iniciar";
             this.btniniciar.UseVisualStyleBackColor = false;
@@ -52,10 +57,12 @@
             // 
             this.ckmostrar.AutoSize = true;
             this.ckmostrar.BackColor = System.Drawing.Color.AliceBlue;
+            this.ckmostrar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
             this.ckmostrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ckmostrar.Location = new System.Drawing.Point(240, 123);
+            this.ckmostrar.Location = new System.Drawing.Point(330, 148);
+            this.ckmostrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ckmostrar.Name = "ckmostrar";
-            this.ckmostrar.Size = new System.Drawing.Size(60, 17);
+            this.ckmostrar.Size = new System.Drawing.Size(97, 27);
             this.ckmostrar.TabIndex = 16;
             this.ckmostrar.Text = "Ocultar";
             this.ckmostrar.UseVisualStyleBackColor = false;
@@ -65,28 +72,32 @@
             // 
             this.txtcontra.BackColor = System.Drawing.Color.AliceBlue;
             this.txtcontra.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.txtcontra.Location = new System.Drawing.Point(134, 120);
+            this.txtcontra.Location = new System.Drawing.Point(179, 148);
+            this.txtcontra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtcontra.Name = "txtcontra";
-            this.txtcontra.Size = new System.Drawing.Size(100, 20);
+            this.txtcontra.Size = new System.Drawing.Size(132, 22);
             this.txtcontra.TabIndex = 15;
             // 
             // txtusuario
             // 
             this.txtusuario.BackColor = System.Drawing.Color.AliceBlue;
             this.txtusuario.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.txtusuario.Location = new System.Drawing.Point(134, 81);
+            this.txtusuario.Location = new System.Drawing.Point(179, 100);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtusuario.Name = "txtusuario";
-            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.Size = new System.Drawing.Size(132, 22);
             this.txtusuario.TabIndex = 14;
             // 
             // lblcontra
             // 
             this.lblcontra.AutoSize = true;
             this.lblcontra.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblcontra.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
             this.lblcontra.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblcontra.Location = new System.Drawing.Point(51, 120);
+            this.lblcontra.Location = new System.Drawing.Point(63, 148);
+            this.lblcontra.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblcontra.Name = "lblcontra";
-            this.lblcontra.Size = new System.Drawing.Size(61, 13);
+            this.lblcontra.Size = new System.Drawing.Size(108, 23);
             this.lblcontra.TabIndex = 13;
             this.lblcontra.Text = "Contraseña";
             // 
@@ -94,26 +105,41 @@
             // 
             this.lblusuario.AutoSize = true;
             this.lblusuario.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblusuario.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
             this.lblusuario.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblusuario.Location = new System.Drawing.Point(69, 81);
+            this.lblusuario.Location = new System.Drawing.Point(96, 98);
+            this.lblusuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblusuario.Name = "lblusuario";
-            this.lblusuario.Size = new System.Drawing.Size(43, 13);
+            this.lblusuario.Size = new System.Drawing.Size(75, 23);
             this.lblusuario.TabIndex = 12;
             this.lblusuario.Text = "Usuario";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 341);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
             // usuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 278);
+            this.ClientSize = new System.Drawing.Size(468, 342);
             this.Controls.Add(this.btniniciar);
             this.Controls.Add(this.ckmostrar);
             this.Controls.Add(this.txtcontra);
             this.Controls.Add(this.txtusuario);
             this.Controls.Add(this.lblcontra);
             this.Controls.Add(this.lblusuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "usuarios";
             this.Text = "usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,5 +153,6 @@
         private System.Windows.Forms.TextBox txtusuario;
         private System.Windows.Forms.Label lblcontra;
         private System.Windows.Forms.Label lblusuario;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
